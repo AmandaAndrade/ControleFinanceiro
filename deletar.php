f714a1a0eff586457ce56955db205ab50ada84e7
AmandaAndrade <amandamandrade@gmail.com>
2016-09-13 13:08:15 -0300

    Delete deletar.php

--- deletar.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "conecta_banco.inc";
-if($_GET['act'] == 'rem'){
-	$id = $_GET['id'];
-	mysql_query("DELETE FROM usuarios WHERE id = $id");
-	echo "<script>alert('Usuário apagado com sucesso!');";
-	echo "location.href='delUser.php'</script>";
-}
-?>
